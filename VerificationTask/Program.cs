ee15a305fd369fdf20eb35183f45cfb800a7049a
Andrei <fmwar92@gmail.com>
2022-08-07 15:42:49 +0300

    Create string array. Filled with elements whose length is 3 or less

--- VerificationTask/Program.cs
+++ VerificationTask/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] inputArray = { "day", "trunk", "engine", "Hi", "car", "#%1", "_!/", "56" };
+int count = 0; // Длина результирующего массива
+int index = 0; // Индекс первого элемента результирующего массива
+
+/// Ищем элементы удовлятворяющие условию задачи и выводим массив
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3) count++; 
+}
+Console.Write($"Initial array is [{String.Join(", ", inputArray)}]\n");
+
+string[] finalArray = new string[count]; // Объявляем результирующий массив и заполняем его
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        finalArray[index] = inputArray[i];
+        index++;
+    }
+    if (index == count) break;
+}
+Console.WriteLine($"Output array is: [{String.Join(", ", finalArray)}]");
